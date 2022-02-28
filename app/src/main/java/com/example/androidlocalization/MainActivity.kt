5d5bbbafb83ad47bc8702c72c7d5f72c1d5c626f
Perennial2002 <pendragon05082002@gmail.com>
2022-02-28 11:17:48 +0500

    mainActivity updated

--- app/src/main/java/com/example/androidlocalization/MainActivity.kt
+++ app/src/main/java/com/example/androidlocalization/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//version handled
         LocaleManager(this).setLocale(this)
 
         findViewById<MaterialButton>(R.id.btn_open_language).setOnClickListener {
